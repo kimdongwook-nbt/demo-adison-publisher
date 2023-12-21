0db92bc30b33d998d06c3ee68db9858d3c0e5888
kimdongwook-nbt <kim.dongwook@nbt.com>
2023-12-21 16:25:47 +0900

    Admin 컨트롤러 추가, 간단한 어드민 목록 출력

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "admins#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/admins', to: "admins#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
