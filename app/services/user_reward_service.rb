15c9cbd81eb0d01db717a454f4bfad4e609b28b8
kimdongwook-nbt <kim.dongwook@nbt.com>
2023-12-29 10:57:07 +0900

    유저 리워드 적립과 적립 내역 저장을 한 트랜잭션에서 동작하도록 수정

--- app/services/user_reward_service.rb
+++ app/services/user_reward_service.rb
@@ -7,27 +7,29 @@ class UserRewardService
     raise PubErrors::InvalidUid.new unless user.present?
     raise PubErrors::DuplicatedTrxId.new if user.user_rewards.where(trx_id:).any?
 
-    user.add_reward(reward)
-    user.save
-
     issued_key = SecureRandom.uuid
-    user.user_rewards.create(
-      trx_id:,
-      click_key:,
-      uid_hash: uid,
-      advertising_id:,
-      client_platform_type:,
-      campaign_id: campaign_id.to_i,
-      ad_id: ad_id.to_i,
-      event_code:,
-      ad_title:,
-      event_title:,
-      reward: reward.to_i,
-      language:,
-      country:,
-      issued_key:,
-      issued_reward_at: Time.zone.now
-    )
+
+    User.transaction do
+      user.add_reward(reward)
+      user.save
+      user.user_rewards.create(
+        trx_id:,
+        click_key:,
+        uid_hash: uid,
+        advertising_id:,
+        client_platform_type:,
+        campaign_id: campaign_id.to_i,
+        ad_id: ad_id.to_i,
+        event_code:,
+        ad_title:,
+        event_title:,
+        reward: reward.to_i,
+        language:,
+        country:,
+        issued_key:,
+        issued_reward_at: Time.zone.now
+      )
+    end
 
     issued_key
   end
