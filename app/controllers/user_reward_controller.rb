bf0d75ae57b5c71bfec2fef60169cef516da98f7
kimdongwook-nbt <kim.dongwook@nbt.com>
2024-01-04 20:52:43 +0900

    유저 리워드 검색 조건 추가

--- app/controllers/user_reward_controller.rb
+++ app/controllers/user_reward_controller.rb
@@ -1,10 +1,10 @@
 class UserRewardController < ApplicationController
   def index
     conditions = search_params
-    @user_rewards = UserReward.retrieve_by(conditions[:trx_id], conditions[:click_key], conditions[:uid_hash], conditions[:ad_title])
+    @user_rewards = UserReward.retrieve_by(conditions[:trx_id], conditions[:click_key], conditions[:uid_hash], conditions[:ad_title], conditions[:country])
   end
 
   def search_params
-    params.permit(:trx_id, :click_key, :uid_hash, :ad_title)
+    params.permit(:trx_id, :click_key, :uid_hash, :ad_title, :country)
   end
 end
--- app/models/user_reward.rb
+++ app/models/user_reward.rb
@@ -8,12 +8,13 @@ class UserReward < ApplicationRecord
   validates :click_key, uniqueness: { scope: :event_code, message: 'click_key and event_code must be unique' }
 
   class << self
-    def retrieve_by(trx_id, click_key, uid_hash, ad_title)
+    def retrieve_by(trx_id, click_key, uid_hash, ad_title, country)
       query = UserReward.order(id: :desc)
       query = query.where("trx_id like ?", "%#{trx_id}%") if trx_id.present?
       query = query.where("click_key like ?", "%#{click_key}%") if click_key.present?
       query = query.where("uid_hash like ?", "%#{uid_hash}%") if uid_hash.present?
       query = query.where("ad_title like ?", "%#{ad_title}%") if ad_title.present?
+      query = query.where("country like ?", "%#{country}%") if country.present?
       query.all
     end
   end
